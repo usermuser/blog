29760eff77aa09636acb8656b6401eb7270641a3
usermuser <lwrdrstv@gmail.com>
2016-05-30 15:55:31 +0500

    page 24. /blog app urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,4 +17,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls',
+                           namespace='blog',
+                           app_name='blog')),
 ]

777f89bc9cccff826856a0f2bfc7492db239e065
usermuser <lwrdrstv@gmail.com>
2016-10-06 17:34:52 +0500

    page 40 almost

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage,\
 PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -43,6 +44,7 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
     if request.method == 'POST':
         # Form was submitted
@@ -51,7 +53,13 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com',[cd['to']])
+            sent = true
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+                                                    'form': form,
+                                                    'sent': sent})

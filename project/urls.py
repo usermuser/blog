43af5f88dc2e4ee6fffaaa582b52bd1706a45162
usermuser <lwrdrstv@gmail.com>
2018-10-30 15:19:59 +0500

    renamed app

--- project/urls.py
+++ project/urls.py
@@ -10,14 +10,14 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add a URL to urlpatterns:  url(r'^blog_app/', include('blog_app.urls'))
 """
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
 #    url(r'^$', views.home, name='home'),
-    url(r'^blog/', include('blog.urls',
+    url(r'^blog/', include('blog_app.urls',
                            namespace='blog_app',
                            app_name='blog_app')),
 #    url(r'^admin/', include(admin.site.urls)),
